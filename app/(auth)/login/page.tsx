2438216d5ecfb20241431cfab15d26eb8a045228
Tegacreatives <contact@tegacreatives.com>
2023-09-04 18:44:00 +0100

    Fixed eslint error

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -75,7 +75,7 @@ const Login = () => {
               </button>
             </div>
             <p className="mt-4 text-sm text-center text-gray-700">
-              Don't have an account?
+              Don&apos;t have an account?{" "}
               <Link
                 href="/signup"
                 className="font-medium text-[#015E5F] hover:underline"
