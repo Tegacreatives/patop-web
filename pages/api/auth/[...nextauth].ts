c29f41509c55ae9da12f8b12e425b2c03a481c81
Tegacreatives <contact@tegacreatives.com>
2023-09-05 20:24:05 +0100

    configured next auth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -56,7 +56,7 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.SECRET,
 };
 
 export default NextAuth(authOptions);
