b321a38f2790e322ae4d11eda74540181e9b9256
Tegacreatives <contact@tegacreatives.com>
2023-10-14 10:16:45 +0100

    Complete project card

--- app/my-campaigns/campaignsClient.tsx
+++ app/my-campaigns/campaignsClient.tsx
@@ -40,22 +40,38 @@ const CampaignsClient = ({ campaigns }: CampaignsClientProps) => {
           const daysLeft = calculateDaysLeft(campaign.endDate);
           return (
             <Link href={`/my-campaigns/${campaign.id}`} key={campaign.id}>
-              <div className=" h-[340px] w-[300px] rounded-t-lg bg-gray-50">
-                <div className=" w-[300px] h-[320px] rounded-t-3xl">
+              <div className=" h-[370px] w-[300px] rounded-lg border border-gray-100">
+                <div className=" w-[280px] pt-3 mx-auto h-[320px] rounded-t-3xl">
                   <div className="relative w-full h-[50%]">
                     <Image
                       src={campaign.imageSrc}
                       alt={campaign.title}
                       fill
-                      className="rounded-t-xl"
+                      className="rounded-xl"
                     />
                   </div>
                   <div className="px-5 pt-3 space-y-3">
                     <h1 className="font-semibold">{campaign.title}</h1>
-                    <h2 className="text-sm text-gray-600">
-                      N300 raised from N{campaign.goalAmount}
-                    </h2>
                     <h2> {daysLeft} Days left</h2>
+                    <div className="flex flex-row items-center justify-between">
+                      <div>
+                        <h3>Raised</h3>
+                        <h2 className="text-sm text-gray-600">N300</h2>
+                      </div>
+                      <div className="w-[1px] h-8 bg-gray-400"></div>
+                      <div>
+                        <h3>Goal</h3>
+                        <h2 className="text-sm text-gray-600">
+                          N{campaign.goalAmount}
+                        </h2>
+                      </div>
+                      <div className="w-[1px] h-8 bg-gray-400"></div>
+                      <div>
+                        <h3>Backers</h3>
+                        <h2 className="text-sm text-gray-600">20+</h2>
+                      </div>
+                    </div>
+
                     <Button label="View Project" />
                   </div>
                 </div>
