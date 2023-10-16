3d65742c763702a7efafe4d06029986ac697997a
Tegacreatives <contact@tegacreatives.com>
2023-10-16 23:19:02 +0100

    updated unique contributors query

--- app/actions/getCampaignById.ts
+++ app/actions/getCampaignById.ts
@@ -15,6 +15,7 @@ export default async function getCampaignById(params: IParams) {
       select: {
         backerId: true,
       },
+      distinct: ["backerId"],
     });
 
     const totalAmountRaised = await prisma.contribution.aggregate({
