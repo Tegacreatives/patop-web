40c8d0966a8a47c5b0a5687dcd1e30c315d3f9aa
Tegacreatives <contact@tegacreatives.com>
2023-10-17 21:28:44 +0100

    Integrated hygraph for blog articles

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,86 @@
+import { RichText } from "@graphcms/rich-text-react-renderer";
+import { EmbedReferences, RichTextContent } from "@graphcms/rich-text-types";
+import Image from "next/image";
+
+async function getBlog(slug: string) {
+  const res = await fetch(process.env.HYGRAPH_ENDPOINT as RequestInfo, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query Blog($slug: String!) {
+            blog(where: {slug: $slug}) {
+                createdAt
+                id
+                slug
+                publishedAt
+                title
+                coverPhoto {
+                  url
+                }
+                content{
+                    markdown
+                    json
+                }
+                
+            }
+        }`,
+      variables: {
+        slug: slug,
+      },
+    }),
+  });
+  const data = await res.json();
+  return data.data.blog;
+}
+
+type blogType = {
+  id: string;
+  slug: string;
+  title: string;
+  publishedAt: string;
+  coverPhoto: {
+    url: string;
+  };
+  content: {
+    json: RichTextContent;
+    references: EmbedReferences;
+  };
+};
+
+const Blog = async ({ params }: { params: { slug: string } }) => {
+  const blogData: blogType = await getBlog(params.slug);
+  const normalDate = new Date(blogData.publishedAt);
+  const formattedDate = normalDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <div className=" ">
+      <main className="prose w-full py-10 px-5 mx-auto max-w-[90vw] md:max-w-[60vw]">
+        <h1 className="text-2xl md:text-4xl font-bold">{blogData.title}</h1>
+        <h3 className="text-sm text-gray-500 py-2">{formattedDate}</h3>
+        <div className="w-[full] h-[40vh] md:h-[60vh] my-10 relative">
+          <Image src={blogData.coverPhoto.url} alt={blogData.title} fill />
+        </div>
+        {blogData.content && (
+          <RichText
+            content={blogData?.content?.json}
+            references={blogData?.content?.references}
+            renderers={{
+              h3: ({ children }) => (
+                <h1 className=" pt-3 text-2xl">{children}</h1>
+              ),
+            }}
+          />
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Blog;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,86 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Blog = () => {
-  return <div>Blog</div>;
+// Get all the courses
+async function getCourses() {
+  const response = await fetch(process.env.HYGRAPH_ENDPOINT as RequestInfo, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+         query Blogs {
+           blogs {
+           createdAt
+           id
+           publishedAt
+           slug
+           title
+           updatedAt
+           coverPhoto {
+           url
+        }
+      }
+    }
+     `,
+    }),
+  });
+  const json = await response.json();
+
+  return json.data.blogs;
+}
+
+type blogType = {
+  id: string;
+  slug: string;
+  title: string;
+  publishedAt: string;
+  coverPhoto: {
+    url: string;
+  };
 };
 
-export default Blog;
+const BlogPage = async () => {
+  const blogs: blogType[] = await getCourses();
+  console.log(blogs);
+  return (
+    <main className="w-full py-10 px-10 md:px-32 mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {blogs.map((blog) => {
+          const normalDate = new Date(blog.publishedAt);
+          const formattedDate = normalDate.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          });
+
+          return (
+            <div
+              className="mx-auto w-[80vw] h-[250px] md:w-[280px] md:h-[230px] lg:w-[370px] lg:h-[350px]"
+              key={blog.id}
+            >
+              <Link href={`/blog/${blog.slug}`}>
+                <div className=" w-full h-full relative rounded-lg">
+                  <Image
+                    src={blog.coverPhoto.url}
+                    alt={blog.title}
+                    fill
+                    className="rounded-lg"
+                  />
+                </div>
+                <h3 className="text-sm text-gray-500 py-2">{formattedDate}</h3>
+                <h2 className="font-semibold truncate text-2xl text-gray-700">
+                  {blog.title}
+                </h2>
+              </Link>{" "}
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default BlogPage;
