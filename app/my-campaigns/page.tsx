775fa659fcf858090bb83d98956a46a27680b98b
Tegacreatives <contact@tegacreatives.com>
2023-10-18 14:17:15 +0100

    Increase campaign page height

--- app/my-campaigns/page.tsx
+++ app/my-campaigns/page.tsx
@@ -7,7 +7,7 @@ const MyCampaigns = async () => {
   const userCampaigns = await getUserCampaigns();
 
   return (
-    <section className="md:px-10 lg:px-20 py-10 max-w-[2180px]">
+    <section className="md:px-10 lg:px-20 py-10 max-w-[2180px] min-h-[80vh]">
       <CampaignTitle />
       <CampaignsClient campaigns={userCampaigns} />
     </section>
