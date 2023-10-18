34c1413900c24f0267b48475f9aca15592db100a
Tegacreatives <contact@tegacreatives.com>
2023-10-18 16:01:04 +0100

    Improved progress bar display

--- components/contributionProgressBar.tsx
+++ components/contributionProgressBar.tsx
@@ -17,7 +17,10 @@ const ContributionProgressBar = ({
         <div className="flex mb-2 items-center justify-between">
           <div>
             <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200">
-              {progress.toFixed(2)}%
+              {totalAmountRaised > remainingAmountNeeded
+                ? 100
+                : progress.toFixed(2)}
+              %
             </span>
           </div>
         </div>
@@ -28,14 +31,24 @@ const ContributionProgressBar = ({
             </span>
           </div>
           <div className="text-left">
-            <span className="text-xs font-semibold inline-block text-teal-600">
-              {remainingAmountNeeded} NGN
-            </span>
+            {totalAmountRaised > remainingAmountNeeded ? (
+              <span className="text-xs font-semibold inline-block text-teal-600">
+                {totalAmountRaised} NGN
+              </span>
+            ) : (
+              <span className="text-xs font-semibold inline-block text-teal-600">
+                {remainingAmountNeeded} NGN
+              </span>
+            )}
           </div>
         </div>
         <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-teal-200">
           <div
-            style={{ width: `${progress}%` }}
+            style={{
+              width: `${
+                totalAmountRaised > remainingAmountNeeded ? 100 : progress
+              }%`,
+            }}
             className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"
           ></div>
         </div>
