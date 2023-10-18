6f1dc2fd4f8fd570c6fdfe98b8e4f0a81bdf5a70
Tegacreatives <contact@tegacreatives.com>
2023-10-18 15:36:49 +0100

    Hide navbar when route changes

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Button } from "../button/Button";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { FiMenu } from "react-icons/fi";
 import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
@@ -32,12 +32,24 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   const router = useRouter();
+  const pathName = usePathname();
   const [isOpen, setIsOpen] = useState(false);
+  const [toogleNav, setToogleNav] = useState(false);
 
   //Toggles the navigation menu
   const toogleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
+
+  const toogleNavMenu = useCallback(() => {
+    setToogleNav((value) => !value);
+  }, []);
+
+  useEffect(() => {
+    setToogleNav(false);
+    setIsOpen(false);
+  }, [pathName]);
+
   return (
     <div
       className="flex items-center justify-between py-6 px-10 md:px-20 top-0
@@ -96,9 +108,52 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         )}
       </div>
-      <div className="block lg:hidden">
+      <div onClick={toogleNavMenu} className="block lg:hidden">
         <FiMenu size={30} />
       </div>
+      {toogleNav && (
+        <div className="absolute lg:hidden bg-white min-w-[80vw] p-4 rounded-xl shadow-md overdlow-hidden right-3 top-28 text-lg z-30">
+          <div className="flex flex-col cursor-pointer justify-between items-start">
+            <ul className="space-y-8 font-medium">
+              {Links.map((link, index) => (
+                <li key={index}>
+                  <Link href={link.url} className="text-[#015E5F]">
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+
+            <div
+              onClick={() => router.push("/my-campaigns")}
+              className="text-[#015E5F] font-medium cursor-pointer my-8"
+            >
+              My Projects
+            </div>
+
+            {currentUser ? (
+              <div
+                onClick={() => signOut()}
+                className="text-[#015E5F] cursor-pointer border border-[#015E5F] px-10  py-3"
+              >
+                Sign out
+              </div>
+            ) : (
+              <>
+                <Button
+                  onClick={() => router.push("/login")}
+                  outline
+                  label="Login"
+                />
+                <Button
+                  onClick={() => router.push("/signup")}
+                  label="Sign Up"
+                />
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
